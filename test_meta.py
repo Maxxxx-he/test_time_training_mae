fd0ad1b76211fd9d36e5a11b6409b8aff90cba71
Haoyu He <wikohuang123@gmail.com>
2024-05-06 17:11:17 -0400

    Update test_meta.py

--- test_meta.py
+++ test_meta.py
@@ -32,6 +32,7 @@ from scipy import stats
 import util.misc as misc
 from util.misc import NativeScalerWithGradNormCount as NativeScaler
 
+
 def get_args_parser():
     parser = argparse.ArgumentParser('MAE testing.', add_help=False)
     # Model parameters
@@ -75,8 +76,9 @@ def get_args_parser():
     parser.add_argument('--optimizer_type', default='sgd', help='adam, adam_w, sgd.')
     parser.add_argument('--optimizer_momentum', default=0.9, type=float, help='adam, adam_w, sgd.')
     parser.add_argument('--finetune_mode', default='encoder', type=str, help='all, encoder, encoder_no_cls_no_msk.')
-
+    parser.add_argument('--epochs', default=100, type=int)
     return parser
+
 
 def get_parameters_from_args(model, args):
     if args.finetune_mode == 'encoder':
@@ -92,6 +94,7 @@ def get_parameters_from_args(model, args):
                 p.requires_grad = False
         parameters = [p for p in model.parameters() if p.requires_grad]
     return parameters
+
 
 def _reinitialize_model(base_model, base_optimizer, base_scalar, clone_model, args, device):
     clone_model.load_state_dict(copy.deepcopy(base_model.state_dict()))
@@ -136,8 +139,10 @@ def main(args):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.676, 0.566, 0.664], std=[0.227, 0.253, 0.217])])
     # dataset_val = datasets.ImageFolder(args.data_path, transform=transform_val)
-    dataset_train = torchvision.datasets.PCAM(root="/home/h_haoy/Myproject/Myprojectpcam/Pcam", split='test',
+    dataset_train = torchvision.datasets.PCAM(root="/home/h_haoy/Myproject/Myprojectpcam/Pcam", split='train',
                                               transform=transform_train, download=False)
+    dataset_val = torchvision.datasets.PCAM(root="/home/h_haoy/Myproject/Myprojectpcam/Pcam", split='val',
+                                            transform=transform_train, download=False)
     # dataset_val = datasets.ImageFolder('/home/h_haoy/Myproject/Pcam/testttt', transform=transform_val)
     classes = 2
 
@@ -146,13 +151,7 @@ def main(args):
     optimizer = torch.optim.SGD(get_prameters_from_args(model, args), lr=args.lr,
                                 momentum=args.optimizer_momentum)
     _ = model.to(args.device)
-    data_len = len(dataset_train)
     # optimizer = None
-    meta_loss = 0
-    all_acc = []
-    all_losses = []
-    all_acc2 = []
-    all_losses2 = []
 
     classifier_embed_dim = 768
     classifier_depth = 12
@@ -163,63 +162,92 @@ def main(args):
                                                          classifier_embed_dim=classifier_embed_dim,
                                                          classifier_num_heads=classifier_num_heads,
                                                          rotation_prediction=False)
+    # args.epochs
+    best_val_loss = float('inf')
+    counter = 0
+    max_patience = 5
+    for epoch in range(args.epochs):
+        meta_loss = 0
+        all_acc = []
+        all_losses = []
+        all_acc2 = []
+        all_losses2 = []
+        model2, optimizer2, loss_scaler2 = _reinitialize_model(model, optimizer, scaler, clone_model, args, args.device)
+        for index in range(len(dataset_train)):
+            # Get the samples:
+            current_idx = index
+            samples, labels = dataset_train[current_idx]
+            samples = samples.to(args.device, non_blocking=True).unsqueeze(0)
+            labels = torch.LongTensor([labels]).to(args.device, non_blocking=True)
+            with torch.no_grad():
+                loss_dict, _, _, pred = model(samples, target=labels, mask_ratio=0)
+                # print(pred)
+                acc1 = (stats.mode(pred.argmax(axis=1).detach().cpu().numpy()).mode[0] == labels[
+                    0].cpu().detach().numpy()) * 100.
+                # loss_dict2, _, _, pred2 = model2(samples, target=labels, mask_ratio=0)
+                # # print(pred)
+                # acc2 = (stats.mode(pred2.argmax(axis=1).detach().cpu().numpy()).mode[0] == labels[
+                #     0].cpu().detach().numpy()) * 100.
+            all_acc.append(acc1)
+            all_losses.append(float(loss_dict['classification'].detach().cpu().numpy()))
+            # all_acc2.append(acc2)
+            # all_losses2.append(float(loss_dict2['classification'].detach().cpu().numpy()))
 
-    model2, optimizer2, loss_scaler2 = _reinitialize_model(model, optimizer, scaler, clone_model, args, args.device)
-    # while meta_loss >= 0.01:
-    #     meta_loss = 0
-    for index in range(data_len):
-        # Get the samples:
-        current_idx = index
-        samples, labels = dataset_train[current_idx]
-        samples = samples.to(args.device, non_blocking=True).unsqueeze(0)
-        labels = torch.LongTensor([labels]).to(args.device, non_blocking=True)
-        
-        with torch.no_grad():
-            loss_dict, _, _, pred = model(samples, target=labels, mask_ratio=0)
-            # print(pred)
-            acc1 = (stats.mode(pred.argmax(axis=1).detach().cpu().numpy()).mode[0] == labels[
-                0].cpu().detach().numpy()) * 100.
-            loss_dict2, _, _, pred2 = model2(samples, target=labels, mask_ratio=0)
-            # print(pred)
-            acc2 = (stats.mode(pred2.argmax(axis=1).detach().cpu().numpy()).mode[0] == labels[
-                0].cpu().detach().numpy()) * 100.
-        all_acc.append(acc1)
-        all_losses.append(float(loss_dict['classification'].detach().cpu().numpy()))
-        all_acc2.append(acc2)
-        all_losses2.append(float(loss_dict2['classification'].detach().cpu().numpy()))
+            #meta training
+            model2.train()
+            mask_ratio = args.mask_ratio
+            # samples, _ = train_data
+            targets_rot, samples_rot = None, None
+            loss_d, _, _, _ = model2(samples, target=None, mask_ratio=mask_ratio)
+            loss = torch.stack([loss_d[l] for l in loss_d]).sum()
+            loss_value = loss.item()
+            # loss /= accum_iter
+            if not math.isfinite(loss_value):
+                print("Loss is {}, stopping training".format(loss_value))
+                sys.exit(1)
+            #print(scaler)
+            loss_scaler2(loss, optimizer2, parameters=model2.parameters(),
+                         update_grad=(index + 1) % 1 == 0)  #binary upgrade
 
-        #
-        # #meta
-        #
-        # model.train()
-        # mask_ratio = args.mask_ratio
-        # # samples, _ = train_data
-        # targets_rot, samples_rot = None, None
-        # loss_dict, _, _, _ = model(samples, target=None, mask_ratio=mask_ratio)
-        # loss = torch.stack([loss_dict[l] for l in loss_dict]).sum()
-        # loss_value = loss.item()
-        # # loss /= accum_iter
-        # if not math.isfinite(loss_value):
-        #     print("Loss is {}, stopping training".format(loss_value))
-        #     sys.exit(1)
-        # #print(scaler)
-        # scaler(loss, optimizer, parameters=model.parameters(),
-        #        update_grad=(1 + 1) % 1 == 0)
-        #
-        # #testing
-        # with torch.no_grad():
-        #     model.eval()
-        #     all_pred = []
-        #     loss_d, _, _, _ = model(samples, labels, mask_ratio=0, reconstruct=False)
-        #     loss_test = torch.stack([loss_d[l] for l in loss_d]).sum()
-        #     loss_test_meta = loss_test.item()
-        #     model.train()
-        # meta_loss += loss_test_meta
+            #testing
+            with torch.no_grad():
+                model2.eval()
+                loss_d, _, _, _ = model2(samples, labels, mask_ratio=0, reconstruct=False)
+                loss_test = torch.stack([loss_d[l] for l in loss_d]).sum()
+                loss_test_meta = loss_test.item()
+                model2.train()
+            meta_loss += loss_test_meta
 
-        if index % 50 == 1:
-            print('step: {}, before {},loss{}'.format(index, np.mean(all_acc[-50:]), np.mean(all_losses[:])))
-            print('step: {}, after {},loss{}'.format(index, np.mean(all_acc2[-50:]), np.mean(all_losses2[:])))
-            #print("loss", np.mean(all_losses[-1000:]))
+        scaler(meta_loss, optimizer, parameters=model.parameters(), update_grad=True)
+        for i in range(len(dataset_val)):
+            # Get the samples:
+            samples2, labels2 = dataset_val[i]
+            samples2 = samples2.to(args.device, non_blocking=True).unsqueeze(0)
+            labels2 = torch.LongTensor([labels2]).to(args.device, non_blocking=True)
+            with torch.no_grad():
+                loss_dict2, _, _, pred2 = model(samples2, target=labels2, mask_ratio=0)
+                # print(pred)
+                acc2 = (stats.mode(pred.argmax(axis=1).detach().cpu().numpy()).mode[0] == labels2[
+                    0].cpu().detach().numpy()) * 100.
+            all_acc2.append(acc2)
+            val_loss = float(loss_dict2['classification'].detach().cpu().numpy())
+            all_losses2.append(val_loss)
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                counter = 0
+            else:
+                counter += 1
+                if counter >= max_patience:
+                    print('Early stopping', epoch)
+                    if args.output_dir:
+                        misc.save_model(
+                            args=args, model=model, model_without_ddp=model, optimizer=optimizer,
+                            loss_scaler=scaler, epoch=epoch)
+                    break
+        # if index % 50 == 1:
+        print('step: {}, train evaluate{},loss{}'.format(epoch, np.mean(all_acc[:]), np.mean(all_losses[:])))
+        print('step: {}, val evaluate {},loss{}'.format(epoch + 1, np.mean(all_acc2[:]), np.mean(all_losses2[:])))
+        #print("loss", np.mean(all_losses[-1000:]))
         #model, optimizer, scaler = load_combined_model(args, classes)
         #optimizer = torch.optim.SGD(get_prameters_from_args(model, args), lr=args.lr,momentum=args.optimizer_momentum)
         #_ = model.to(args.device)
@@ -227,9 +255,9 @@ def main(args):
     print('Saving to', os.path.join(args.output_dir, 'accuracy.txt'))
     with open(os.path.join(args.output_dir, 'accuracy.txt'), 'a') as f:
         f.write(f'{str(args)}\n')
-        f.write(f'{np.mean(all_acc)} {np.mean(all_losses)}\n')
-    with open(os.path.join(args.output_dir, 'accuracy.npy'), 'wb') as f:
-        np.save(f, np.array(all_results))
+        f.write(f'{np.mean(all_acc)} {np.mean(all_losses)} {np.mean(all_acc2)} {np.mean(all_losses2)}\n')
+    # with open(os.path.join(args.output_dir, 'accuracy.npy'), 'wb') as f:
+    #     np.save(f, np.array(all_results))
 
 
 if __name__ == '__main__':
